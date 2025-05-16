b043bae9651d3ef976b4add7010d4392951ed2f0
Antrax <fredcox@gmail.com>
2025-05-16 12:11:59 -0300

    ok

--- packages.sh
+++ packages.sh
@@ -16,7 +16,7 @@ PACKAGES=(
   gdu bat fuzzel waybar nwg-look imagemagick swaync yt-dlp kdenlive
   telegram-desktop dosfstools firefox pavucontrol-qt
   evince network-manager-applet net-tools btop wget 7zip stow
-  entr ristretto
+  entr ristretto man-db man-pages
   ##########################################################################################
   # zsh
   ##########################################################################################
