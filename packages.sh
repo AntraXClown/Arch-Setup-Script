f4931a945e47d212884cc57506160621d1b325be
antrax <fredcox@gmail.com>
2025-06-26 14:05:30 -0300

    kvantum added

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 PACKAGES=(
   ##########################################################################################
   # Themes, Cursors and Fonts
@@ -20,7 +20,7 @@ PACKAGES=(
   telegram-desktop dosfstools firefox pavucontrol-qt
   evince network-manager-applet net-tools btop wget 7zip stow
   entr ristretto man-db man-pages tldr cava rsync swaybg swayidle
-  legcord nfs-utils xorg-xhost nvidia-settings
+  legcord nfs-utils xorg-xhost nvidia-settings kvantum
   ##########################################################################################
   # niri
   ##########################################################################################
